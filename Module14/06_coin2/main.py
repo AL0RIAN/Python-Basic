1f596a5a5163d87ff01aef4a107576ba901cedc8
AL0RIAN <slowpok19450509@gmail.com>
2021-08-16 15:21:30 +0300

    The task 6 is done

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def coin_detector(x, y, r):
+    if x <= r and y <= r:
+        print('Монетка где-то рядом')
+    else:
+        print('Монетки в области нет')
+
+
+x = float(input('X: '))
+y = float(input('Y: '))
+r = float(input('Введите радиус: '))
+coin_detector(x, y, r)
