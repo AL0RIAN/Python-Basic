9b0511d4be7f6f198b2385b5baaa00c48c882b7b
AL0RIAN <slowpok19450509@gmail.com>
2021-08-16 16:04:53 +0300

    The task 7 is done

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def special_year(a, b):
+    for year in range(a, b + 1):
+        for number in str(year):
+            if str(year).count(number) == 3:
+                print(year)
+                break
+
+
+a = int(input('Первый год: '))
+b = int(input('Второй год: '))
+special_year(a, b)
