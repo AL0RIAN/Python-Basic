aaf772d3d06bbb3d6908cbd96753027c0cad9a57
AL0RIAN <slowpok19450509@gmail.com>
2021-08-16 15:01:34 +0300

    The task 4 is done

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,18 @@
-# TODO здесь писать код
+def reverse_float(number):
+    switcher = 0
+    res1 = ''
+    res2 = ''
+    for symbol in str(number):
+        if symbol == '.':
+            switcher = 1
+        elif switcher == 0:
+            res1 = symbol + res1
+        else:
+            res2 = symbol + res2
+    return float(res1 + '.' + res2)
 
 
+n = float(input('Введите первое число: '))
+k = float(input('Введите второе число: '))
+print('\nПервое число наоборот:', reverse_float(n))
+print('Второе число наоборот:', reverse_float(k))
