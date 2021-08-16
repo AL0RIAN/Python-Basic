8f4f8f37b833fa6be528d5e45c8ec36b8e977647
AL0RIAN <slowpok19450509@gmail.com>
2021-08-16 15:11:06 +0300

    The task 5 is done

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def least_divisor(n):
+    for number in range(2, n + 1):
+        if n % number == 0:
+            return number
+
+
+n = int(input('Введите число: '))
+print('Наименьший делитель, отличный от единицы:', least_divisor(n))
