99d087b6a49225de00407fe0f255df4d07d7b0b1
AL0RIAN <slowpok19450509@gmail.com>
2021-08-19 12:39:55 +0300

    The task 5 is done

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,14 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+favorite = []
 
-# TODO здесь писать код
+while True:
+    film = input('Фильм: ')
+    if film in films:
+        favorite.append(film)
+    else:
+        print('Ошибка')
+        break
+
+print('Любимые фильмы:', favorite)
