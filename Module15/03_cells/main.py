25b3722fdc8c0d874c040f16e5f309afb01e6923
AL0RIAN <slowpok19450509@gmail.com>
2021-08-19 11:40:21 +0300

    The task 3 is done

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+n = int(input('Сколько клеток: '))
+cells = []
+
+print('Кол-во клеток:', n)
+
+for i in range(n):
+    print('Эффективность', i + 1, 'клетки: ', end='')
+    cell = int(input())
+    cells.append(cell)
+
+for index in range(n):
+    if cells[index] < index:
+        print('\nНеподходящие значения:', cells[index], end=' ')
