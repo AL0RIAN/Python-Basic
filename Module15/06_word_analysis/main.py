2d9166549fba4272c9910e79b0133f20f144dc52
AL0RIAN <slowpok19450509@gmail.com>
2021-08-19 13:54:55 +0300

    The task 6 is done

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+lit_list = []
+exceptions = []
+
+for sym in word:
+    c = 0
+    for i in word:
+        if sym == i:
+            c += 1
+        if c >= 2:
+            exceptions.append(sym)
+
+for sym in word:
+    if sym not in exceptions:
+        lit_list.append(sym)
+
+print('Кол-во уникальных букв:', len(lit_list))
