1aa873c69a62798119a9e2e8fba9417c79c7ea3f
AL0RIAN <slowpok19450509@gmail.com>
2021-08-19 11:31:49 +0300

    The task 2 is done

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+members = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+index = 0
+
+for member in members:
+    if index % 2 == 0:
+        print(members[index], end=', ')
+    index += 1
+
+
