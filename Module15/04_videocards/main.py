1d907a2e0f6ebf7733d8b1013da4b4bdec4843f0
AL0RIAN <slowpok19450509@gmail.com>
2021-08-19 12:27:38 +0300

    The task 4 is done

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,21 @@
-# TODO здесь писать код
+n = int(input('Кол-во видеокарт: '))
+videocards = []
 
+for i in range(n):
+    print(i + 1, 'Видеокарта:', end=' ')
+    videocard = int(input())
+    videocards.append(videocard)
 
+maximum = videocards[0]
+new_videcards = []
+
+for i in videocards:
+    if i > maximum:
+        maximum = i
+
+for i in videocards:
+    if i != maximum:
+        new_videcards.append(i)
+
+print('\nСтарый список видеокарт:', videocards)
+print('Новый список видеокарт:', new_videcards)
