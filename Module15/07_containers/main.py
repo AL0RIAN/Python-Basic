9bb6ac5896a71f13b9ed6d3ed9c29ee353077611
AL0RIAN <slowpok19450509@gmail.com>
2021-08-20 16:00:43 +0300

    The task 7 is done

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+n = int(input('Кол-во контейнеров: '))
+containers = []
+left_border = []
+right_border = []
+
+for _ in range(n):
+    cont = int(input('Введите вес контейнера: '))
+    containers.append(cont)
+
+new_cont = int(input('\nВведите вес нового контейнера: '))
+
+for cont in containers:
+    if cont >= new_cont:
+        left_border.append(cont)
+    else:
+        right_border.append(cont)
+
+containers = left_border + [new_cont] + right_border
+
+if len(left_border) > 0 and len(right_border) > 0:
+    print('\nНомер, куда встанет новый контейнер:', len(left_border) + 1)
+elif len(left_border) == 0:
+    print('\nНомер, куда встанет новый контейнер: 1')
+elif len(right_border) == 0:
+    print('\nНомер, куда встанет новый контейнер:', len(left_border) + 1)
