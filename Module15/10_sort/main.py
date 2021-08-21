80a95796c1be95ea91431b3a2f254f78a270f8b0
AL0RIAN <slowpok19450509@gmail.com>
2021-08-21 22:58:18 +0300

    The task 10 is done

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+# сортировка пузырьком
+
+n = int(input('Кол-во элементов в списке: '))
+origin = []
+
+for _ in range(n):
+    number = int(input('Элемент списка: '))
+    origin.append(number)
+
+print('Изначальный список:', origin)
+
+for _ in range(len(origin) - 1):
+    flag = 0
+    for index in range(0, len(origin) - 1):
+        if origin[index] > origin[index + 1]:
+            a = origin[index]
+            b = origin[index + 1]
+            origin[index] = b
+            origin[index + 1] = a
+            flag = 1
+    if flag == 0:
+        break
+
+print('Отсортированный список:', origin)
