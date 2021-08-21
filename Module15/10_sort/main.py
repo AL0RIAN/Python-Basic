ffe7ec03105a1593784378dfd908a8c5ea4c0a55
AL0RIAN <slowpok19450509@gmail.com>
2021-08-21 22:58:18 +0300

    The task 10 is done

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+# сортировка пузырьком
+
+origin = [1, 4, -3, 0, 10]
+
+print('Изначальный список:', origin)
+
+for _ in range(len(origin) - 1):
+    flag = 0
+    for index in range(0, len(origin) - 1):
+        if origin[index] > origin[index + 1]:
+            a = origin[index]
+            b = origin[index + 1]
+            origin[index] = b
+            origin[index + 1] = a
+            flag = 1
+    if flag == 0:
+        break
+
+print('Отсортированный список:', origin)
