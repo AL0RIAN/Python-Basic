c717576df1c647436071564fd19433a61014c8d3
AL0RIAN <slowpok19450509@gmail.com>
2021-08-21 22:36:40 +0300

    The task 9 is done

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+word = list(input('Слово: '))
+reverse_word = []
+
+for index in range(1, len(word) + 1):
+    reverse_word.append(word[-index])
+
+if reverse_word == word:
+    print('\nСлово является палиндромом')
+else:
+    print('\nСлово не является палиндромом')
