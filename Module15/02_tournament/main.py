445eabe835fb67267412f2fa97cb163e92263d60
Alexander Olhovik <aao.t@yandex.ru>
2021-08-22 22:33:15 +0300

    урок 15, зачет (замечания в 2)

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -3,7 +3,7 @@ index = 0
 
 for member in members:
     if index % 2 == 0:
-        print(members[index], end=', ')
+        print(members[index], end=', ')  # TODO member содержит именно то, что в members[index] :)
     index += 1
 
-
+# TODO другой вариант - итерировать по индексам с шагом 2, тогда if не понадобится
