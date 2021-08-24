7321b1b786195241387bac563bb365d1be2a3d2a
AL0RIAN <slowpok19450509@gmail.com>
2021-08-24 14:17:04 +0300

    The task 4 is done

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,30 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+def count_of_guests(list_of_guests):
+    if len(list_of_guests) < 6:
+        return True
+    else:
+        return False
 
-# TODO здесь писать код
+
+guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+condition = ''
+
+while True:
+    print('\nСейчас на вечеринке', len(guests), 'человек:', guests)
+    condition = input('Гость пришел или ушел? ')
+    if condition == 'пришел':
+        guest = input('Имя гостя: ')
+        if count_of_guests(guests):
+            print('Привет,', guest)
+            guests.append(guest)
+        else:
+            print('Прости,', guest, 'но мест нет')
+    elif condition == 'ушел':
+        guest = input('Имя гостя: ')
+        print('Пока,', guest, '!')
+        guests.remove(guest)
+    elif condition == 'Пора спать':
+        break
+    else:
+        print('Неверный ввод!')
+
+print('\nВечеринка закончилась, все легли спать.')
