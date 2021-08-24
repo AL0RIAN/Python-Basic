11b5de7553f396c9d29ad2bcc5df1b14c879e5c4
AL0RIAN <slowpok19450509@gmail.com>
2021-08-24 15:00:20 +0300

    The task 6 is done

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+first = []
+second = []
+for _ in range(3):
+    number = int(input('Целое число в первый список: '))
+    first.append(number)
+print()
+
+for _ in range(7):
+    number = int(input('Целое число во второй список: '))
+    second.append(number)
+
+print('\nПервый список:', first)
+print('Второй список:', second)
+first.extend(second)
+
+for number in first:
+    for _ in range(first.count(number) - 1):
+        first.remove(number)
+
+print('\nНовый первый список с уникальными элементами:', first)
