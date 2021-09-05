23ee44f6dab52fc86beaa611865811b59541cc72
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-05 13:16:32 +0300

    The task 8 is done

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def circle_destroyer(m):
+    for _ in range(k):
+        if m == len(circle) - 1:
+            m = 0
+        else:
+            m += 1
+    print('Выбывает человек под номером', circle[m])
+    circle.remove(circle[m])
+
+
+n = int(input('Кол-во человек: '))
+k = int(input('Какое число в считалке? '))
+circle = list(range(1, n + 1))
+print('Значит, выбывает каждый', k, 'человек')
+
+while len(circle) > 1:
+    print('\nТекущий круг людей:', circle)
+    member = int(input('Начало счёта с номера: '))
+    member = circle.index(member) - 1
+    circle_destroyer(member)
+
+print('\nОстался человек под номером', circle[0])
