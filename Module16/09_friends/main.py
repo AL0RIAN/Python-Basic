7f9e1c63f97da96d422992db17a6fadd879a4610
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-06 01:20:02 +0300

    The task 9 is done

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+n = int(input('Кол-во друзей: '))
+k = int(input('Кол-во долговых расписок: '))
+money = []
+
+for _ in range(n):
+    money.append(0)
+
+for receipt in range(k):
+    print()
+    print(receipt + 1, 'расписка')
+    debtor = int(input('Кому: '))
+    creditor = int(input('От кого: '))
+    how_money = int(input('Сколько: '))
+    money[debtor - 1] -= how_money
+    money[creditor - 1] += how_money
+
+print('\nБаланс друзей:')
+for friend in range(n):
+    print(friend + 1, ':', money[friend])
