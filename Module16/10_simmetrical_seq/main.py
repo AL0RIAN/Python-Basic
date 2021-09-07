3d998390fe6d00a91fd55f32cfec33559e443211
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-07 13:31:39 +0300

    The task 10 is done

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+def palindrom(num_list):
+    reverse = []
+    for i_num in range(len(num_list) - 1, -1, -1):
+        reverse.append(num_list[i_num])
+    if num_list == reverse:
+        return True
+    else:
+        return False
+
+
+n = int(input('Кол-во чисел: '))
+lst = []
+new_nums = []
+res = []
+
+for index in range(n):
+    print('Число:', index + 1, end=' ')
+    number = int(input())
+    lst.append(number)
+
+for i_nums in range(0, len(lst)):
+    for j_elem in range(i_nums, len(lst)):
+        new_nums.append(lst[j_elem])
+
+    if palindrom(new_nums):
+        for i_answer in range(0, i_nums):
+            res.append(lst[i_answer])
+        res.reverse()
+        break
+    new_nums = []
+
+print('\nПоследовательность:', lst)
+print('Нужно приписать чисел:', len(res))
+print('Сами числа::', res)
