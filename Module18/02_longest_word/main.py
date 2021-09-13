620fadcbc69e4cbbe36f8f3f4e5a97002bc4b59f
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-13 13:34:47 +0300

    The task 2 is done

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+text = input('Строка: ')
+res = ['', 0]
+print(text)
+
+for word in text.split():
+    if len(word) > res[1]:
+        res[1] = len(word)
+        res[0] = word
+
+print('\nСамое длинное слово - {0}, а его длина {1}!'.format(res[0], res[1]))
