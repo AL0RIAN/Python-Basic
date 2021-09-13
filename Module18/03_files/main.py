ff91576ed4b18c6e6e6abb190ddf59dc681fde36
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-13 14:01:25 +0300

    The task 3 is done

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+text = input('Название файла: ')
+
+if text[0] in '@№$%^&*()':
+    print('Ошибка: название начинается на один из специальных символов')
+elif not text.endswith('.txt') and not text.endswith('.docx'):
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+else:
+    print('Файл назван верно')
