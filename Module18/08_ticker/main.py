3550c58ab5443b9532fd6789b729e84ca284d2ff
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-13 16:48:03 +0300

    The task 8 is done

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+first = input('Первая строка: ')
+second = input('Вторая строка: ')
+
+for index in range(len(first)):
+    if first[-index::] + first[:-index] == second:
+        print('Первая строка получается из второй со сдвигом:', index)
+        break
+else:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига')
