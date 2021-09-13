0ea108537875a4fe587fc6d01ef830fd4c5b6a09
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-13 15:09:47 +0300

    The task 5 is done

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+while True:
+    password = input('Придумайте пароль: ')
+    print('Пароль ненадёжный. Попробуйте ещё раз.')
+    up = 0
+    digit = 0
+    for symbol in password:
+        if symbol.isupper():
+            up += 1
+        if symbol.isdigit():
+            digit += 1
+    if len(password) >= 8 and digit >= 3 and up >= 1:
+        print('Это надежный пароль!')
+        break
+
