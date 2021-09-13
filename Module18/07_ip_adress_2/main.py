2ab413cfa1a6f0cb0b6bf38956e2f6b225e57c60
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-13 16:29:19 +0300

    The task 7 is done

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+ip = input('\nВведите IP: ').split('.')
+for symbol in ip:
+    if len(ip) < 4:
+        print('Адрес - это четыре числа, разделенные точками')
+        break
+    elif not symbol.isdigit():
+        print('{} - не целое число'.format(symbol))
+        break
+    elif int(symbol) > 255:
+        print('{} превышает 255'.format(symbol))
+        break
+else:
+    print('IP-адрес корректен')
+
