637eb1a8fad0e3ee98aa4dd92780a801abcdbd37
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-14 19:06:30 +0300

    The task 6 is done

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+s = input('Введите строку: ')
+res = []
+
+while s:
+    count = 0
+    now = s[0]
+    for symbol in s:
+        if symbol == now:
+            count += 1
+        else:
+            break
+    res.extend([now, str(count)])
+    s = s[count:]
+
+print('Закодированная строка:', ''.join(res))
