0783febbf9f5f3e3fdb504301f570c17c3d52c15
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-14 17:08:11 +0300

    The task 9 is done

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+message = input('Сообщение: ').split()
+res = []
+
+for word in message:
+    alpha = ''
+    new_word = ''
+    for symbol in word:
+        if symbol.isalpha():
+            alpha = symbol + alpha
+        else:
+            new_word += alpha + symbol
+            alpha = ''
+    new_word += alpha
+    res.append(new_word)
+
+print('Новое сообщение:', ' '.join(res))
