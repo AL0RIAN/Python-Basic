4842d2b5c06f316a5a3c2612634dcd789d525c37
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-14 23:05:28 +0300

    The task 10 is done

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf ' \
+       'jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp ' \
+       'djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme ' \
+       'wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv ' \
+       'Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc ' \
+       'bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ' \
+       'ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf ' \
+       'b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+first = ''
+second = ''
+for symbol in text:
+    if symbol in alphabet:
+        number = alphabet.find(symbol)
+        first += alphabet[number - 1]
+    else:
+        first += symbol
+
+replace_index = 3
+
+for word in first.split(' '):
+    new_word = ''
+    for index in range(len(word)):
+        new_word += (word[index - replace_index % len(word)])
+    if new_word.endswith('/'):
+        replace_index += 1
+        new_word = new_word[:-1]
+        new_word += '\n'
+    second += new_word + ' '
+
+print('Расшифровка:', second)
