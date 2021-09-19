98f5fdff791b588c0411188b0024b2e75dfcde30
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-19 21:52:01 +0300

    The task 2 is done

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+country_count = int(input('Кол-во стран: '))
+country_lst = []
+
+for number in range(country_count):
+    print('{} страна: '.format(number + 1), end='')
+    country = input().split()
+    country_lst.append(country)
+
+country_dict = {country_lst[index][0]: country_lst[index][1:] for index in range(2)}
+
+for index in range(3):
+    print('\n{} город'.format(index + 1), end=': ')
+    city = input()
+    for country in country_dict:
+        if city in country_dict.get(country):
+            print('Город {0} расположен в стране {1}'.format(city, country))
+            break
+    else:
+        print('По городу {} нет данных'.format(city))
+
