e0ffecabd56b433d60439a12dfb9be4145cc6fe5
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-20 17:08:08 +0300

    The task 5 is done

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+text = 'Здесь что-то написано'
+frequency = {symbol: text.count(symbol) for symbol in text}
+
+for key in frequency:
+    print('{0} : {1}'.format(key, frequency[key]))
+print()
+
+result = []
+for index in range(1, max(frequency.values()) + 1):
+    for key in frequency:
+        if frequency[key] == index:
+            result.append(key)
+    print('{0} : {1}'.format(index, result))
+    result = []
