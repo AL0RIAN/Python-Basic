4b1a853f97e38a2566e3bcf275c4ffff6c3a40b7
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-20 15:36:56 +0300

    The task 3 is done

--- Module19/03_cryptocurrency/main.py
+++ Module19/03_cryptocurrency/main.py
@@ -44,5 +44,23 @@ data = {
     ]
 }
 
+print('1. Вывести списки ключей и значений словаря')
+print(data.keys())
+print(data.values())
 
-# TODO здесь писать код
+print('\n2. В “ETH” добавить ключ “total_diff” со значением 100')
+data["ETH"]["total_diff"] = 100
+
+print('\n3. Внутри “fst_token_info” значение ключа “name” поменять с “fdf” на “doge”')
+data["tokens"][0]["fst_token_info"]["name"] = "doge"
+
+print('\n4. Удалить “total_out” из tokens и присвоить его значение в “total_out” внутри “ETH”')
+copy = data["tokens"][0]['total_out']
+data["tokens"][0].pop("total_out")
+data["ETH"]["total_out"] = copy
+
+print('\n5. Внутри "sec_token_info" изменить название ключа “price” на “total_price”.')
+data["tokens"][1]['sec_token_info']["total_price"] = data["tokens"][1]['sec_token_info'].pop("price")
+
+print('\nРеузльтат: {}'.format(data))
+
