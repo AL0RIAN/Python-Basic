fb959e5de696982f4a7ff5e99f18ebbff15005b3
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-22 01:59:04 +0300

    The task 8 is done

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+n = int(input('Введите максимальное число: '))
+result = {'right': set()}
+
+while True:
+    numbers = input('\nНужное число есть среди вот этих чисел: ').lower()
+    if numbers == 'помогите!':
+        break
+    yesNo = input('Ответ Артёма: ').lower()
+    lst = [int(number) for number in numbers.split()]
+    if yesNo == 'да':
+        result['right'] = result['right'] | set(lst)
+    elif yesNo == 'нет':
+        result['right'] = result['right'] - set(lst)
+    else:
+        print('Неверный ввод')
+
+print('\nАртём мог загадать следующие числа: ', end='')
+for number in result['right']:
+    print(number, end=' ')
