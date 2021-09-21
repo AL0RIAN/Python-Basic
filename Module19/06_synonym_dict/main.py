1cc349de53ed453e8f242f553af4994db7344c98
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-22 00:55:49 +0300

    The task 6 is done

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def checker(dictionary):
+    check = input('Введите слово: ').lower()
+    if check in result:
+        print('Синоним: {}'.format(result[check].capitalize()))
+    elif check in result.values():
+        for key in dictionary:
+            if dictionary[key] == check:
+                print('Синоним: {}'.format(key.capitalize()))
+    else:
+        print('Такого слова в словаре нет. Попробуйте ещё раз')
+        checker(dictionary)
+
+
+n = int(input('Кол-во пар: '))
+result = dict()
+
+for index in range(n):
+    print(index + 1, 'пара:', end=' ')
+    word = input().lower().split(' - ')
+    result.setdefault(word[0], word[1])
+print()
+
+for _ in range(n):
+    checker(result)
