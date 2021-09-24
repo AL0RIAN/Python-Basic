8ccf8eddd58c92f382c96c60cc8bdb371f0f0081
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-24 20:55:00 +0300

    The task 10 is done

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+
+for index in range(1, len(text) + 1):
+    if text == text[::-1]:
+        print('Можно сделать палиндромом')
+        break
+    text = text[-1:] + text[0:-1]
+else:
+    print('Нельзя сделать палиндромом')
+
