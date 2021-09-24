3f5fad93d23a3da5818186679f4e0c0da861d295
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-24 21:01:17 +0300

    The task 7 is done

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+n = int(input('Кол-во заказов: '))
+result = dict()
+
+for index in range(n):
+    print(index + 1, 'заказ:', end=' ')
+    order = input().split()
+    if order[0] not in result.keys():
+        result[order[0]] = {order[1]: int(order[2])}
+    elif order[1] in result[order[0]]:
+        result[order[0]][order[1]] += int(order[2])
+    else:
+        result[order[0]].update({order[1]: int(order[2])})
+
+for key1 in sorted(result.keys()):
+    print('{}:'.format(key1))
+    for key2 in result[key1]:
+        print('{0}{1}: {2}'.format(' ' * 7, key2, result[key1].get(key2)))
