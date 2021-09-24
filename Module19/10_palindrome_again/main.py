ec010d76036571ff4be9fbfe9bc5e1d647e17d5a
kirill_solotopov <slowpok19450509@gmail.com>
2021-09-24 21:34:46 +0300

    The task 10 is done (another solution)

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,10 +1,26 @@
+def palindrome(string):
+    char = {}
+    for i in string:
+        char[i] = char.get(i, 0) + 1
+
+    odd = 0
+    for i in char.values():
+        if i % 2 != 0:
+            odd += 1
+    return odd <= 1
+
+
 text = input('Введите строку: ')
 
-for index in range(1, len(text) + 1):
-    if text == text[::-1]:
-        print('Можно сделать палиндромом')
-        break
-    text = text[-1:] + text[0:-1]
+if palindrome(text):
+    print('Можно сделать палиндромом')
 else:
     print('Нельзя сделать палиндромом')
 
+# for index in range(1, len(text) + 1):
+#     if text == text[::-1]:
+#         print('Можно сделать палиндромом')
+#         break
+#     text = text[-1:] + text[0:-1]
+# else:
+#     print('Нельзя сделать палиндромом')
