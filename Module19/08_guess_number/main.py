d68a784528a889a8321ff792ee80a6349f11c05b
Alexander Olhovik <aao.t@yandex.ru>
2021-09-26 22:00:03 +0300

    урок 19, зачет (недочеты в 8)

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,5 +1,5 @@
 n = int(input('Введите максимальное число: '))
-result = {'right': set()}
+result = {'right': set()}  # TODO почему множество пустое?
 
 while True:
     numbers = input('\nНужное число есть среди вот этих чисел: ').lower()
@@ -8,7 +8,7 @@ while True:
     yesNo = input('Ответ Артёма: ').lower()
     lst = [int(number) for number in numbers.split()]
     if yesNo == 'да':
-        result['right'] = result['right'] | set(lst)
+        result['right'] = result['right'] | set(lst)  # TODO нужно пересечение множеств, а не объединение
     elif yesNo == 'нет':
         result['right'] = result['right'] - set(lst)
     else:
