1c063567a074806c1f2751786a082a7e52e0c5c6
AL0RIAN <slowpok19450509@gmail.com>
2021-10-11 15:29:47 +0300

    The task 3 is done

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def func(tpl, elem):
+    interval = [index for index, item in enumerate(tpl) if item == elem]
+    if tpl.count(elem) == 2:
+        return tpl[interval[0]:interval[1] + 1]
+    elif tpl.count(elem) == 1:
+        return tpl[interval[0]:]
+    else:
+        return ()
+
+
+numbers = (1, 1, 0, 1, 1, 1, 1, 0, 1, 1)
+print(func(numbers, 0))
+
