6d77abeddb48ec9b759644f74ac783b49b48aafa
AL0RIAN <slowpok19450509@gmail.com>
2021-10-14 19:50:06 +0300

    The task 9 is done

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+n = int(input("Кол-во строк протокола: "))
+lst = {}
+
+for index in range(n):
+    print(f"{index + 1} запись:", end=' ')
+    score, member = input().split()
+    score = int(score)
+    if member in lst.keys():
+        if lst[member] < score:
+            lst[member] = score
+        else:
+            pass
+    else:
+        lst[member] = score
+print()
+
+scores = sorted(lst.values())
+
+for position in range(3):
+    for winner in lst.keys():
+        if lst[winner] == scores[-1]:
+            print("{0} место. {1} ({2})".format(position + 1, winner, scores[-1]))
+            scores.pop()
+            break
