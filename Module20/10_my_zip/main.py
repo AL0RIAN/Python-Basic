ac8bcb32cf35856fa0644eab511000bc57edbe7f
AL0RIAN <slowpok19450509@gmail.com>
2021-10-16 13:37:57 +0300

    The task 10 is done

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def shortest(str, tpl):
+    return min(len(str), len(tpl))
+
+
+text = 'abcdfc'
+numbers = (10, 20, 30, 40)
+
+new = ((text[i], numbers[i]) for i in range(shortest(text, numbers)))
+print(new)
+for item in new:
+    print(item)
