4f23fd57f3430bb0b8b280f89981e981d14ca134
slowpok19450509@gmail.com <D19450509>
2021-12-12 02:32:10 +0200

    The task #7 is done

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,31 @@
+def deepcopy(inList):
+    if isinstance(inList, list):
+        return list( map(deepcopy, inList) )
+    return inList
+
+# def deepcopy(struct, copy, deep=0):
+#     for value in struct:
+#         if isinstance(struct[value], dict):
+#             copy[value] = deepcopy(struct[value], copy)
+#             return struct[value]
+#         else:
+#             return struct[value]
+
+
+def rebranding(struct, brand_name):
+    if 'title' in struct:
+        struct['title'] = 'Куплю/продам {} недорого'.format(brand_name)
+        return True
+    elif 'h2' in struct:
+        struct['h2'] = 'У нас самая низкая цена на {}'.format(brand_name)
+        return True
+    for value in struct.values():
+        if isinstance(value, dict):
+            if rebranding(value, brand_name):
+                break
+    return struct
+
+
 site = {
     'html': {
         'head': {
@@ -11,4 +39,15 @@ site = {
     }
 }
 
-# TODO здесь писать код
+# count = int(input("Введите кол-во брендов: "))
+count = 1
+brands = {}
+# for counter in range(count):
+# brand = input("Введите название продукта для нового сайта: "
+
+# deepcopy(site, {})
+# print(deepcopy(site, {}))
+brands['samsung'] = rebranding(deepcopy(site, {}), 'samsung')
+print(brands['samsung'])
+print(site)
+# НАМ НУЖНО СДЕЛАТЬ "ГЛУБОКУЮ КОПИЮ" С ПОМОЩЬЮ РЕКУРСИИ.
