f7470e9609518b538607080364ecbc4ed1c6a393
slowpok19450509@gmail.com <D19450509>
2021-12-14 02:58:11 +0200

    The task #8 is done

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,13 @@
+def straight(struct, new=[]):
+    for value in struct:
+        if isinstance(value, list):
+            straight(value)
+        else:
+            new.append(value)
+    return new                               
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+print('Ответ:', straight(nice_list))
