58674bdaef02a3966150ba57cf092ccb2fc9f177
slowpok19450509@gmail.com <D19450509>
2022-01-28 19:41:38 +0200

    The task 3 is done

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+import os
+
+file = open(os.path.abspath(os.path.join("..", "02_zen_of_python", "zen.txt")), "r")
+letters = 0
+words = 0
+lines = 0
+rarest_letter = {}
+
+for letter in file.read():
+    letters += len(letter)
+
+file.seek(0)
+
+for word in file.readlines():
+    words += len(word.split(' '))
+
+file.seek(0)
+
+for line in file.readlines():
+    lines += 1
+
+file.seek(0)
+
+for sym in file.read():
+    if sym in rarest_letter:
+        rarest_letter[sym.lower()] += 1
+    else:
+        if sym.isalpha():
+            rarest_letter[sym.lower()] = 1
+
+file.close()
+
+print("Буквы:", letters)
+print("Слова:", words)
+print("Строки:", lines)
+print("Самая редкая буква в тексте:", min(rarest_letter))
