f669914b6636a89015c4f448740ad0b757995fa9
slowpok19450509@gmail.com <D19450509>
2022-01-28 19:34:28 +0200

    The task 2 is done

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+count = 0
+file = open("zen.txt", 'r')
+count = 1
+temp = 19
+while temp:
+    for line in file.readlines():
+        if count == temp:
+            if '\n' not in line:
+                line += '\n'
+            print(line, end='')
+        count += 1
+    count = 1
+    temp -= 1
+    file.seek(0)
