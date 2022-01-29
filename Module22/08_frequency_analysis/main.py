eea28873787f627be7c3a426aed71ac1d5c6de04
slowpok19450509@gmail.com <D19450509>
2022-01-29 05:12:47 +0200

    The task 8 is done

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+letters = {}
+total = 0
+
+file_text = open('text.txt', 'r')
+for letter in file_text.read():
+    if letter.isalpha():
+        total += 1
+        if letter.lower() in letters:
+            letters[letter.lower()] += 1
+        else:
+            letters[letter.lower()] = 1
+
+temp_score = 0
+temp_member = ''
+members = []
+count = []
+
+while letters:
+    for sym in sorted(letters):
+        if letters[sym] > temp_score:
+            temp_score = letters[sym]
+            temp_member = sym
+    count.append(temp_score)
+    members.append(temp_member)
+    letters.pop(temp_member)
+    temp_score = 0
+    temp_member = ''
+
+file_text.close()
+
+file_analysis = open('analysis.txt', 'w')
+for elem in range(len(members)):
+    file_analysis.write('{} {:.3f}'.format(members[elem], (count[elem] / total)) + '\n')
+file_analysis.close()
