f4b84fd27cf0498d8686baac14035ddfbe644ed9
slowpok19450509@gmail.com <D19450509>
2022-01-29 04:02:59 +0200

    The task 6 is done

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def caesar_cipher(word, step):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    new_word = ''
+    for letter in word:
+        if letter != '\n':
+            new_word += (alphabet[(alphabet.index(letter.lower()) + step) % 25])
+        else:
+            break
+    return new_word
+
+
+count = 1
+text_file = open('text.txt', 'r')
+cipher_file = open('cipher_text.txt', 'w')
+
+for line in text_file.readlines():
+    cipher_file.write(caesar_cipher(line, count) + '\n')
+    count += 1
+
+text_file.close()
+cipher_file.close()
