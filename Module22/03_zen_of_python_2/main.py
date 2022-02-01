4782a4b965ca26b8aec51147e756c2e58866f47b
slowpok19450509@gmail.com <D19450509>
2022-02-02 01:55:17 +0200

    The task #3 (fix)

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -30,9 +30,17 @@ for sym in file.read():
 
 file.close()
 
+print(rarest_letter)
+
 print("Буквы:", letters)
 print("Слова:", words)
 print("Строки:", lines)
-print("Самая редкая буква в тексте:", min(rarest_letter))  # TODO вывели "миниммальную" букву (по её коду), а не самую
-                                                           #  редкую. Надо проитерировать по словарю и найти запись с
-                                                           #  минимальным значением
+print("Самая редкая буква в тексте:", end=' ')
+
+min_value = max(rarest_letter.values())
+rarest_key = 0
+for letter in rarest_letter:
+    if rarest_letter[letter] < min_value:
+        rarest_key = letter
+        min_value = rarest_letter[letter]
+print(rarest_key)
