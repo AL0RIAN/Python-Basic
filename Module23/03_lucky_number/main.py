c3ef4ba994ce3529a510539d8982a9df85b1f813
slowpok19450509@gmail.com <D19450509>
2022-02-10 16:21:53 +0200

    The task 3 is done

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import random
+
+suma = 0
+
+with open('lucky.txt', 'w') as lucky_file:
+    while suma < 777:
+        number = input('Введите число: ')
+        chance = random.randint(1, 13)
+        try:
+            if chance == 1:
+                raise Exception
+            else:
+                lucky_file.write(number + '\n')
+                suma += int(number)
+        except Exception:
+            print('Вас постигла неудача!')
+            break
+    else:
+        print('Вы успешно выполнили условие для выхода из порочного цикла!')
