21db30f86cfd6db5d3eca35e2f3b53e41553ee2f
slowpok19450509@gmail.com <D19450509>
2022-02-10 16:00:19 +0200

    The task 2 is done

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -1,7 +1,7 @@
 import random
 
 
-def f(x, y):
+def f1(x, y):
     x += random.randint(0, 10)
     y += random.randint(0, 5)
     return x / y
@@ -13,27 +13,15 @@ def f2(x, y):
     return y / x
 
 
-try:
-    file = open('coordinates.txt', 'r')
+with open('coordinates.txt', 'r') as file:
     for line in file:
         nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
         try:
+            res1 = f1(int(nums_list[0]), int(nums_list[1]))
             res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
-
-
-# TODO отредактировать и исправить программу
+        except ZeroDivisionError:
+            print('Error: division by zero!')
+        number = random.randint(0, 100)
+        with open('result.txt', 'a') as file_2:
+            my_list = sorted([res1, res2, number])
+            file_2.write(f'{str(my_list[0])} {str(my_list[1])} {str(my_list[2])}' + '\n')
