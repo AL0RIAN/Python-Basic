ed1fbd55308c10c00bb1fc92b6b0d32a54d6e525
slowpok19450509@gmail.com <D19450509>
2022-02-11 00:38:28 +0200

    The task 5 is done

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def text_calc(operand1, sign, operand2):
+    if sign == '+':
+        return operand1 + operand2
+    elif sign == '-':
+        return operand1 - operand2
+    elif sign == '*':
+        return operand1 * operand2
+    elif sign == '/':
+        try:
+            if operand1 != 0:
+                return operand1 / operand2
+            else:
+                raise ZeroDivisionError
+        except ZeroDivisionError:
+            print("Ошибка: деление на ноль!")
+    else:
+        return False
+
+
+with open('calc.txt', 'r') as file:
+    for line in file.readlines():
+        if text_calc(int(line.split()[0]), line.split()[1], int(line.split()[2])):
+            print(text_calc(int(line.split()[0]), line.split()[1], int(line.split()[2])))
