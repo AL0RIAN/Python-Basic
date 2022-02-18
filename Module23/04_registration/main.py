e860f90e4c5bd6fed64d6ddb5b78b1a2343e611f
slowpok19450509@gmail.com <D19450509>
2022-02-18 17:28:50 +0200

    The task 4 is fixed

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -1,28 +1,20 @@
 def correct(data):
-    try:
-        error = ''
-        if len(data) != 3:
-            error += 'IndexError: НЕ присутствуют все три поля: '
-            raise IndexError
-        elif not data[0].isalpha():
-            error += 'NameError: Поле имени содержит НЕ только буквы'
-            raise NameError
-        elif not ('@' in data[1] and '.' in data[1]):
-            error += 'SyntaxError: Поле емейл НЕ содержит @ и .(точку)'
-            raise SyntaxError
-        elif not (10 <= int(data[2]) < 99):
-            error += 'ValueError: Поле возраст НЕ является числом от 10 до 99'
-            raise ValueError
-    except BaseException:
-        return error
-    return ''
+    if len(data) != 3:
+        raise IndexError('IndexError: НЕ присутствуют все три поля: ')
+    elif not data[0].isalpha():
+        raise NameError('NameError: Поле имени содержит НЕ только буквы')
+    elif not ('@' in data[1] and '.' in data[1]):
+        raise SyntaxError('SyntaxError: Поле емейл НЕ содержит @ и .(точку)')
+    elif not (10 <= int(data[2]) < 99):
+        raise ValueError('ValueError: Поле возраст НЕ является числом от 10 до 99')
 
 
 with open('registrations.txt', 'r', encoding='utf-8') as file:
     with open('registrations_bad.log', 'w', encoding='utf-8') as bad:
         with open('registrations_good.log', 'w', encoding='utf-8') as good:
             for line in file.readlines():
-                if len(correct(line.split())) == 0:
+                try:
+                    correct(line.split())
                     good.write(f'{"".join(line)}')
-                else:
-                    bad.write(f'{"".join(line)} --- {correct(line)}')
+                except (IndexError, NameError, SyntaxError, ValueError) as error:
+                    bad.write(f'{line.rstrip()} - {error.__class__.__name__} - {error}\n')
