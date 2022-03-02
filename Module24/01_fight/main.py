47aec9acc7dbecfa16669b3b87226e773dc7b4f8
slowpok19450509@gmail.com <D19450509>
2022-03-02 16:55:25 +0200

    The task 1 is done

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+from random import randint
+
+
+class Unit:
+    health = 100
+
+    def __init__(self, name='Воин'):
+        self.name = name
+
+    def get_damage(self):
+        self.health -= 20
+
+
+unit_1 = Unit('Герберт')
+unit_2 = Unit('Роберт')
+while unit_1.health != 0 and unit_2.health != 0:
+    initiative = randint(1, 2)
+    print(f'Бросок 1к2: {initiative}')
+    if initiative == 1:
+        print(f'{unit_1.name} атакует по {unit_2.name} и наносит 20 единиц повреждений')
+        unit_2.get_damage()
+        print(f'У {unit_2.name} осталось {unit_2.health} очков здоровья\n')
+    else:
+        print(f'{unit_2.name} атакует по {unit_1.name} и наносит 20 единиц повреждений')
+        unit_1.get_damage()
+        print(f'У {unit_1.name} осталось {unit_1.health} очков здоровья\n')
