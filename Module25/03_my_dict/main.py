386f6d9e7ccbb20ed2569d52f96a62c1e884cc41
slowpok19450509@gmail.com <D19450509>
2022-03-21 17:48:40 +0200

    The task 3 is done

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key):
+        for i_key in self.keys():
+            if key == i_key:
+                return i_key
+        else:
+            return 0
+
+
+if __name__ == '__main__':
+    dict_1 = MyDict()
+    dict_1['A'] = 'B'
+    print(dict_1.get('A'))
+    print(dict_1.get('B'))
