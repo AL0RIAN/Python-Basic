f81771d891f82b3bc0fb87e293856c207f6e431e
slowpok19450509@gmail.com <D19450509>
2022-03-29 17:12:44 +0300

    Fixed and docstring added

--- Module25/05_auto/main.py
+++ Module25/05_auto/main.py
@@ -1,70 +1,92 @@
+import math
+
+
 class Auto:
+    """
+    Родительский класс, описывающий автомобиль
+
+    Args:
+        x (int): координата по икс
+        y (int): координата по игрек
+        angle (int): угол
+    """
+
     def __init__(self, x=0, y=0, angle=0):
         self.x = x
         self.y = y
         self.angle = angle
 
     def __str__(self):
-        return f'X: {self.x}, Y: {self.y}, ANGLE: {self.angle}'
+        return f'X: {round(self.x, 2)}, Y: {round(self.y, 2)}, ANGLE: {self.angle}'
 
-    def move(self):
-        count = int(input('Какое расстояние едем: '))
-        if 0 < self.angle < 90:
-            self.x += count
-            self.y += count
-        elif 90 < self.angle < 180:
-            self.x -= count
-            self.y += count
-        elif 180 < self.angle < 270:
-            self.x -= count
-            self.y -= count
-        elif 270 < self.angle < 360:
-            self.x += count
-            self.y -= count
-        elif self.angle == 0 or self.angle == 360:
-            self.x += count
-        elif self.angle == 90:
-            self.y += count
-        elif self.angle == 180:
-            self.x -= count
-        elif self.angle == 270:
-            self.y -= count
-        return count
+    def move(self, count):
+        """
+        Метод расчета конечных координат авто при движении
+
+        :param count: расстояние
+        :type: int
+        """
+        self.x += count * math.cos(self.angle)
+        self.y += count * math.sin(self.angle)
 
     def turn(self, angle):
+        """
+        Метод изменения угла
+
+        :param angle: угол
+        :type: int
+        """
         self.angle = angle
-        print()
 
 
 class Bus(Auto):
+    """
+    Дочерний класс, описывающий автобус. Родитель: Auto
+
+    Attributes:
+        passenger (int): кол-во пассажиров
+        money (int): кол-во денег
+    """
     passenger = 0
     money = 0
-
-    def __init__(self, x, y, angle):
-        super().__init__(x, y, angle)
 
     def __str__(self):
         info = super().__str__()
         return f'{info}. MONEY: {self.money}, PASSENGER: {self.passenger}\n'
 
-    def enter(self):
-        count = int(input('Сколько пассажиров садится: '))
+    def enter(self, count):
+        """
+        Метод прибавления входящих пассажиров к атрибуту passenger
+        В случае достижения лимита выводится соответсвующее сообщение
+
+        Args:
+            count (int): кол-во входящих пассажиров
+        """
         self.passenger += count
         if self.passenger > 30:
             self.passenger = 30
             print('Максимальное количество пассажиров!')
-        print()
 
-    def exit(self):
-        count = int(input('Сколько пассажиров выходит: '))
+    def exit(self, count):
+        """
+        Метод убавления выходящих пассажиров из атрибута passenger
+
+        Args:
+            count (int): кол-во выходящих пассажиров
+        """
         self.passenger -= count
         if self.passenger < 0:
             self.passenger = 0
-        print()
 
-    def move(self):
-        self.money += self.passenger * super().move()
-        print()
+    def move(self, count):
+        """
+        Метод расчета конечных координат автобуса при движении и выручки
+
+        :param count: расстояние
+        :type: int
+        """
+        super().move(count)
+        self.money += self.passenger * count
 
 
 if __name__ == '__main__':
@@ -84,14 +106,17 @@ if __name__ == '__main__':
         print()
 
         if choice == 1:
-            bus.enter()
+            count_input = int(input('Сколько пассажиров заходят: '))
+            bus.enter(count=count_input)
         elif choice == 2:
-            bus.move()
+            count_input = int(input('Какое расстояние едем: '))
+            bus.move(count=count_input)
         elif choice == 3:
-            angle_new = int(input('Угол поворота: '))
+            angle_new = int(input('Новый угол: '))
             bus.turn(angle_new)
         elif choice == 4:
-            bus.exit()
+            count_input = int(input('Сколько пассажиров выходит: '))
+            bus.exit(count=count_input)
         elif choice == 5:
             break
         else:
