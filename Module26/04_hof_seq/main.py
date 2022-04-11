72cbf71be3bc7fade639388320c1395f06f2fb18
slowpok19450509@gmail.com <D19450509>
2022-04-11 21:05:04 +0300

    Task 4 is done

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,6 +1,30 @@
 class QHofstadter:
-    pass
+    def __init__(self, lst: list) -> None:
+        self.lst = lst
+
+    def __iter__(self) -> any:
+        return self
+
+    def __next__(self) -> int:
+        try:
+            q1 = self.lst[-1]
+            q2 = self.lst[-2]
+            self.lst.append(q1 + q2)
+            return q1 + q2
+        except IndexError:
+            raise StopIteration()
 
 
-# Нужна подсказка: что обозначает список из двух элементов и почему при [1, 2] последовательность должна завершиться?
-# Не могу понять до какого и с какого момента должна генерироваться последовательность.
+if __name__ == '__main__':
+    Q = QHofstadter([1, 1])
+    while True:
+        choice = int(input('1 - следующий элемент\n2 - вывести текущие элементы\n3 - выход из цикла\nВвод: '))
+        if choice == 1:
+            print(f'\nСледующий элемент: {next(Q)}\n')
+        elif choice == 2:
+            print(f'\nВесь список: {Q.lst}\n')
+        elif choice == 3:
+            print('\nКонец программы')
+            break
+        else:
+            raise KeyError
