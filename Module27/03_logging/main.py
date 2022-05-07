759402efd3a54dcf07102ef649a8bf89ceaeb915
Kirill <slowpok19450509@gmail.com>
2022-05-07 18:41:20 +0300

    Task 3 done

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,1 +1,44 @@
-# TODO здесь писать код
+import datetime
+import functools
+from typing import Callable, Any
+
+
+def logging(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs) -> Any:
+        try:
+            print(f'Function name: {func.__name__}\n'
+                  f'Documentation: {func.__doc__}\n\n'
+                  )
+            result = func(*args, **kwargs)
+            return result
+        except Exception as error:
+            with open('function_errors.log', 'a') as file:
+                file.write(
+                    f'Time: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}\n'
+                    f'Function name: {func.__name__}\n'
+                    f'Error name: {error.__class__.__name__}\n\n'
+                )
+
+    return wrapped
+
+
+@logging
+def test(num: int) -> (bool, KeyError):
+    """If num = 1 it returns True"""
+    if num == 1:
+        return True
+    elif num == 2:
+        raise EOFError
+    elif num == 3:
+        raise IndexError
+    else:
+        raise KeyError
+
+
+if __name__ == "__main__":
+    test(0)
+    test(1)
+    test(1)
+    test(2)
+    test(3)
