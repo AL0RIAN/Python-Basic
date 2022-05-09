fb453e1c51663cf4a5a75a0ac00c2797590856d9
Kirill <slowpok19450509@gmail.com>
2022-05-09 19:36:31 +0300

    Task 5 done

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+import functools
+from typing import Callable, Any
+
+
+def counter(func: Callable) -> Callable:
+    """Counts number of the function calls """
+    @functools.wraps(func)
+    def wrapped(cache={}, *args, **kwargs) -> Any:
+        if func.__name__ in cache.keys():
+            cache[func.__name__] += 1
+        else:
+            cache[func.__name__] = 1
+        result = func(*args, **kwargs)
+        print(f'Функция {func.__name__} была вызвана {cache[func.__name__]} раз(а)\n')
+        return result
+
+    return wrapped
+
+
+@counter
+def test_1():
+    """Output '<Тут что-то происходит...>'"""
+    print('<Тут что-то происходит...>')
+
+
+@counter
+def test_2():
+    """Output '<Тут ничего не происходит...>'"""
+    print('<Тут ничего не происходит...>')
+
+
+if __name__ == "__main__":
+    test_1()
+    test_1()
+    test_1()
+    test_2()
+    test_2()
+    test_2()
