226bde318ddcd427ef4d437e9861562c0eda086a
Kirill <slowpok19450509@gmail.com>
2022-07-08 15:34:54 +0300

    Task 2 done

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+app = {}
+
+
+def callback(route: str) -> Callable:
+    def wrapper(func: Callable) -> Callable:
+        app[route] = func
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result
+
+        return wrapped
+
+    return wrapper
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get("//")
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print("Такого пути нет")
