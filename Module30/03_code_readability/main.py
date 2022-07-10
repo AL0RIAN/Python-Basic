fda22c91a7389ec0fc21c8d04277b0c007cf1e37
Kirill <slowpok19450509@gmail.com>
2022-07-10 21:37:37 +0300

    Task 3 done

--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import List
+
+print("Вариант 1")
+result1: List[int] = [x for x in range(2, 1001)]
+for i in range(2, 100):
+    result1 = list(filter(lambda x: x == i or x % i, result1))
+print(result1)
+
+print("\nВариант 2")
+numbers = range(2, 1000)
+result2: List[int] = []
+
+for num in numbers:
+    border = num ** 1 // 2
+    flag = True
+    for i in range(2, border + 1):
+        if num % i == 0:
+            flag = False
+            break
+
+    if flag:
+        result2.append(num)
+
+print(result2)
+
