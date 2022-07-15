78c6c9e13e08aabfb5c1934c4a7a130911dea77f
Kirill <slowpok19450509@gmail.com>
2022-07-15 20:50:00 +0300

    Task 5 done

--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import itertools
+
+numbers = list(range(0, 10))
+count = 1
+
+for item in itertools.combinations(numbers, 4):
+    print(f'Вариант {count}: {" ".join(list(map(lambda x: str(x), item)))}')
+    count += 1
