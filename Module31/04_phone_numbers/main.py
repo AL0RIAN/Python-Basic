7d8aa1128a091e954dfa7310bdb4b23f0214f460
Kirill <slowpok19450509@gmail.com>
2022-07-15 20:49:34 +0300

    Task 4 done

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+pattern = r'[89]\d{9}'
+temp = ['9999999999', '999999-999', '99999x9999']
+index = 1
+
+for number in temp:
+    result = re.match(pattern, number)
+    if result:
+        print(f'{index} номер: всё в порядке')
+    else:
+        print(f'{index} номер: не подходит')
+    index += 1
